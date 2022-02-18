a62b15aa94c0ebd8cfa76b2a2a0b0d0694f43560
sumannam <sumannam@gmail.com>
2022-02-18 21:52:29 +0900

    웹 페이지 크롤링 완료

---
+++ Crawling/2. Web Page/1. hollysCrawler.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import pandas as pd
+import datetime
+import os
+
+#[CODE 1]
+def hollys_store(result):
+    for page in range(1, 58): # 게시판의 마지막 숫자만큼 표시
+        Hollys_url = 'https://www.hollys.co.kr/store/korea/korStore.do?pageNo=%d&sido=&gugun=&store=' %page
+        print(Hollys_url)
+        html = urllib.request.urlopen(Hollys_url)
+        soupHollys = BeautifulSoup(html, 'html.parser')
+        tag_tbody = soupHollys.find('tbody')
+
+        for store in tag_tbody.find_all('tr'):
+            if len(store) <= 3:
+                break
+            store_td = store.find_all('td')
+            store_name = store_td[1].string
+            store_sido = store_td[0].string
+            store_address = store_td[3].string
+            store_phone = store_td[5].string
+            result.append([store_name]+[store_sido]+[store_address]
+                          +[store_phone])
+    return
+
+#[CODE 0]
+def main():
+    result = []
+    print('Hollys store crawling >>>>>>>>>>>>>>>>>>>>>>>>>>')
+    
+    hollys_store(result)   #[CODE 1] 호출 
+    hollys_tbl = pd.DataFrame(result, columns=('store', 'sido-gu', 'address','phone'))
+
+    # 현재 경로가 './crawling'이라서 경로 확인과 변경으로 필요
+    print(os.getcwd())
+    os.chdir("2. Web Page")
+
+    hollys_tbl.to_csv('./data/hollys.csv', encoding='cp949', mode='w', index=True)
+    del result[:]
+       
+if __name__ == '__main__':
+     main()
